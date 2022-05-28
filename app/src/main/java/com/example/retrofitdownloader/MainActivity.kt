ed5809312b09cc6ef275a612f06342c6fd234b2e
tejaskhartude <tejaskhartude@gmail.com>
2022-05-28 17:06:26 +0530

    Adding retrofit

--- app/src/main/java/com/example/retrofitdownloader/MainActivity.kt
+++ app/src/main/java/com/example/retrofitdownloader/MainActivity.kt
@@ -3,6 +3,12 @@ package com.example.retrofitdownloader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.retrofitdownloader.databinding.ActivityMainBinding
+import kotlinx.coroutines.coroutineScope
+import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +20,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        with(binding){
+            button.setOnClickListener{
+                  val ok_builder = OkHttpClient.Builder()
+                val retrofit= Retrofit.Builder()
+                         .baseUrl("https://github.com/")
+                         .client(OkHttpClient.Builder().build())
+                         .addCallAdapterFactory(RxJava2CallAdapterFactory.create()).build()
+
+
+
+                val downloadService = retrofit.create(RetrofitInterface::class.java)
+
+
+
+                  downloadService.downloadFile("gameplay3d/GamePlay/archive/master.zip")
+
+
+            }
+        }
+
 
 
 
     }
+
+
 }
--- app/src/main/java/com/example/retrofitdownloader/RetrofitInterface.kt
+++ app/src/main/java/com/example/retrofitdownloader/RetrofitInterface.kt
@@ -4,13 +4,14 @@ import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Streaming
+import retrofit2.http.Url
 
 
 interface RetrofitInterface {
 
     @Streaming
-    @GET("/resource/example.zip")
-    suspend fun downloadFile():Response<ResponseBody>
+    @GET
+    fun downloadFile(@Url fileUrl: String):Response<ResponseBody>
 
 
 }
